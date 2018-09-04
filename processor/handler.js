7bc0c2ecabb2758f8a760edbf4c7fbbda3d127a0
Atheer83 <atheer.for.tech@gmail.com>
2018-09-04 18:35:32 +0300

    llll

--- processor/handler.js
+++ processor/handler.js
@@ -2,9 +2,9 @@
 
 const { TransactionHandler } = require('sawtooth-sdk/processor/handler');
 const { InvalidTransaction } = require('sawtooth-sdk/processor/exceptions');
-const { decode } = require('./services/encoding');
-
-
+const { decode, encode } = require('./services/encoding');
+const { getCollectionAddress, getMojiAddress } = require('./services/addressing')
+const { getPrng } = require('./services/prng')
 const FAMILY_NAME = 'cryptomoji';
 const FAMILY_VERSION = '0.1';
 const NAMESPACE = '5f4d76';
@@ -18,9 +18,9 @@ class MojiHandler extends TransactionHandler {
    * validator, declaring which family name, versions, and namespaces it
    * expects to handle. We'll fill this one in for you.
    */
-  constructor () {
+  constructor() {
     console.log('Initializing cryptomoji handler with namespace:', NAMESPACE);
-    super(FAMILY_NAME, [ FAMILY_VERSION ], [ NAMESPACE ]);
+    super(FAMILY_NAME, [FAMILY_VERSION], [NAMESPACE]);
   }
 
   /**
@@ -46,11 +46,49 @@ class MojiHandler extends TransactionHandler {
    *   - context.deleteState(addresses): deletes the state for the passed
    *     array of state addresses. Only needed if attempting the extra credit.
    */
-  apply (txn, context) {
-    // Enter your solution here
-    // (start by decoding your payload and checking which action it has)
-
+  createThreeMoji() {
+    
   }
+
+  createCollection(context, { name }, signerPublicKey) {
+    const address = getCollectionAddress(signerPublicKey);
+    // const emojiAderess= getMojiAddress(address, getPrng(nmber))
+    console.log(address)
+    return context.getState([address]).then(state => {
+      if (state[address].length > 0) {
+        throw new InvalidTransaction('Owner already exists');
+      }
+      const update = {};
+      update[address] = encode({ key: signerPublicKey,moji:[name] });
+      console.log('ahelellooo worl', update)
+      // update[address]
+      //create 3 mojii // moji array
+      return context.setState(update);
+    });
+  }
+
+
+
+
+  apply(txn, context) {
+    let payload = null;
+    try {
+      payload = decode(txn.payload);
+    } catch (err) {
+      throw new InvalidTransaction('Unable to decode payload');
+    }
+    console.log('hellllllloooo world')
+    if (payload.action === 'CREATE_COLLECTION') {
+      return this.createCollection(context, payload, txn.header.signerPublicKey);
+    }
+
+    throw new InvalidTransaction('Unknown action');
+  }
+
+  // Enter your solution here
+  // (start by decoding your payload and checking which action it has)
+
+
 }
 
 module.exports = MojiHandler;
